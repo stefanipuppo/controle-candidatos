9f54fa911899243b30bc66ff131a577a87a74c1a
Stefani Dal Puppo <stefanipuppo@hotmail.com>
2024-05-08 13:18:01 -0300

    case 2

--- src/candidaturas/ProcessoSeletivo.java
+++ src/candidaturas/ProcessoSeletivo.java
@@ -1,11 +1,37 @@
 package candidaturas;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        selecaoCandidato();
+        
     }
+    static void selecaoCandidato() {
+        String [] candidatos = {"Felipe", "Marcia", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+    
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+        while(candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato" + candidato + " Solicitou este valor de salario" + salarioPretendido);
+            if(salarioBase >= salarioPretendido) {
+                System.out.println("O candidato" + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        
+        }
+    }
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800,2200);
+    }
+
+
+
     static void analisarCandidato(double salarioPretendido) {
         double salarioBase = 2000.0;
         if(salarioBase > salarioPretendido){
