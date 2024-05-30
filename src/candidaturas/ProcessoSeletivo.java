592c1fbd3be53f63c3d5da5f51b31da285d7df83
stefanipuppo <stefanipuppo@hotmail.com>
2024-05-30 20:25:28 -0300

    case 4

--- src/candidaturas/ProcessoSeletivo.java
+++ src/candidaturas/ProcessoSeletivo.java
@@ -1,10 +1,33 @@
 package candidaturas;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-       imprimirSelecionados();
+        String [] candidatos = {"Felipe", "Marcia", "JULIA", "PAULO", "AUGUSTO"};
+        for(String candidato: candidatos) {
+            entrandoEmContato(candidato);
+      }
         
+    }
+    static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando)
+                tentativasRealizadas++;
+            else
+                System.out.println("Contato realizado com sucesso");
+
+        } while (continuarTentando && tentativasRealizadas<3);
+
+    }
+
+    static boolean atender() {
+        return new Random().nextInt(3)==1;
     }
 
     static void imprimirSelecionados() {
